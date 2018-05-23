4c0bdf743c3db531bcc9e2e24cf24e7b9e1adaa4
Marnus Weststrate <marnusw@gmail.com>
2018-05-23 16:07:35 +0100

    CommandBus middleware retries commands when event persistence fails

--- src/CommandBus/CommandHandlerMiddleWare.php
+++ src/CommandBus/CommandHandlerMiddleWare.php
@@ -13,6 +13,9 @@ use SmoothPhp\Contracts\CommandBus\CommandTranslator;
  */
 final class CommandHandlerMiddleWare implements CommandBusMiddleware
 {
+    const ATTEMPTS = 4;
+    const ATTEMPT_INTERVAL = 100;
+
     /** @var CommandTranslator */
     private $commandTranslator;
 
@@ -32,12 +35,36 @@ final class CommandHandlerMiddleWare implements CommandBusMiddleware
     /**
      * @param Command $command
      * @param callable $next
-     * @return mixed|void
+     * @return mixed
+     * @throws \Exception
      */
     public function execute(Command $command, callable $next)
     {
         $handler = $this->commandTranslator->toCommandHandler($command);
 
-        return $this->handlerResolver->make($handler)->handle($command);
+        // It is possible to have contention here when an aggregate is updated simultaneously on
+        // two separate threads. Both will read the existing events at the same time, then add new
+        // events and try to save with the updated play-head. Since both will increment the play-head
+        // by one, the loser will receive a duplicate on unique key exception. Since the command that
+        // succeeded might have change the aggregate in a way that would affect the business logic of
+        // the second it has to start from scratch, loading all events including the one(s) just saved.
+
+        $attempt = 0;
+        $lastError = null;
+
+        while ($attempt++ < self::ATTEMPTS) {
+            try {
+                return $this->handlerResolver->make($handler)->handle($command);
+            } catch (\Exception $exception) {
+                $lastError = $exception;
+                if ($attempt < self::ATTEMPTS) {
+                    usleep(self::ATTEMPT_INTERVAL * $attempt); // linearly increasing back-off
+                } else {
+                    throw $exception;
+                }
+            }
+        }
+
+        throw $lastError;
     }
 }
